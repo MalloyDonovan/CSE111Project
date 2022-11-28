208b9fb5db4311668aeaae463cfdb8fed1f96656
gzarate6 <gzarate6@ucmerced.edu>
2022-11-27 20:10:12 -0800

    Added a button that can print out a query result

--- App.py
+++ App.py
@@ -30,27 +30,52 @@ def closeConnection(_conn, _dbFile):
     print("++++++++++++++++++++++++++++++++++")
 
 
-def main():
-    database = r"tpch.sqlite"
+def q1(_conn):
+    _conn.execute("BEGIN")
+    try:
+        sql = ''' 
+                SELECT g_title FROM Games
+                WHERE strftime('%Y', g_releaseNA) = '2000' AND g_mainStory > 30
+                LIMIT 20;
+            '''
+        _conn.execute(sql)
+        cur = _conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        ###ASSIGN QUERY RESULTS TO BUTTON
+        results["text"] = '\n'.join(''.join(tup) for tup in rows)
+        _conn.execute("COMMIT")
+        print("success")
+    except Error as e:
+        _conn.execute("ROLLBACK")
+        print(e)
+
+
+
+######CAN BE OPTIMIZED BY TAKING THE LIST 
+def handle_click(event):
+    database = r"videogames.db"
 
     # create a database connection
     conn = openConnection(database)
-    #with conn:
-        #dropTable(conn)
-        #createTable(conn)
-        #populateTable(conn)
-
-        #Q1(conn)
-        #Q2(conn)
-        #Q3(conn)
-        #Q4(conn)
-        #Q5(conn)
-
+    q1(conn)
     closeConnection(conn, database)
+
+
 
 #Creates Tkinter window
 window = tk.Tk()
-title = tk.Label(text="Video game database")
-title.pack()
+results = tk.Label(text="Video game database")
+
+button = tk.Button(
+    text="Click to show titles from 2000!",
+    width=25,
+    height=5,
+    bg="black",
+    fg="white",
+)
+
+results.pack()
+button.pack()
+button.bind("<Button-1>", handle_click)
 window.mainloop()
-print("test")
